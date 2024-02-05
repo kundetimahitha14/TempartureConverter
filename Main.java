dddb684749bbbfda8e4bbbcf4d72a15b0a876861
kundetimahitha14 <157417296+kundetimahitha14@users.noreply.github.com>
2024-02-05 14:42:25 -0500

    added 2 java files

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TemperatureConverter tc = new TemperatureConverter();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to Temperature Converter");
+
+        System.out.print("Enter the temperature value: ");
+        double inputValue = scan.nextDouble();
+
+        System.out.println("Select the input scale (C, F, K): ");
+        String inputScale = scan.next();
+
+        double centigrade;
+        double fahrenheit;
+        double kelvin;
+        System.out.println("Temperature Conversion Results:");
+        switch (inputScale) {
+            case "F":
+                centigrade = tc.convertFtoC(inputValue);
+                kelvin = tc.convertCtoK(centigrade);
+                System.out.println("Centigrade: " + centigrade + " C");
+                System.out.println("Kelvin: " + kelvin + " K");
+                break;
+            case "K":
+                centigrade = tc.convertKtoC(inputValue);
+                fahrenheit = tc.convertCtoF(centigrade);
+                System.out.println("Centigrade: " + centigrade + " C");
+                System.out.println("Fahrenheit: " + fahrenheit + " F");
+                break;
+            case "C":
+                centigrade = inputValue;
+                fahrenheit = tc.convertCtoF(centigrade);
+                kelvin = tc.convertCtoK(centigrade);
+                System.out.println("Fahrenheit: " + fahrenheit + " F");
+                System.out.println("Kelvin: " + kelvin + " K");
+                break;
+        }
+
+    }
+}
---
+++ TemperatureConverter.java
@@ -0,0 +1,19 @@
+public class TemperatureConverter {
+
+
+    public double convertFtoC(double fahreinheit) {
+        return (fahreinheit - 32) * 5/9;
+    }
+
+    public double convertKtoC(double kelvin) {
+        return (kelvin - 273.15);
+    }
+
+    public double convertCtoF(double centigrade) {
+        return (centigrade * 9 / 5) + 32;
+    }
+
+    public double convertCtoK(double centigrade) {
+        return (centigrade + 273.15);
+    }
+}
