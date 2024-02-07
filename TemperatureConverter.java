9c21fb608420324d4d449c27c39f5cbc2d10f409
mahitha <kundeti.m14@gmail.com>
2024-02-07 13:05:00 -0500

    Added a sentence in line 8, 12, 16

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -5,15 +5,15 @@ public class TemperatureConverter {
     public double convertFtoC(double fahreinheit) { 
         return (fahreinheit - 32) * 5/9;
     }
-
+// This is my Kelvin to Centigrade conversion code.
     public double convertKtoC(double kelvin) {
         return (kelvin - 273.15);
     }
-
+// This is my centigrade to Fahreinheit conversion code.
     public double convertCtoF(double centigrade) {
         return (centigrade * 9 / 5) + 32;
     }
-
+// This is my centigrade to Kelvin conversion code.
     public double convertCtoK(double centigrade) {
         return (centigrade + 273.15);
     }
